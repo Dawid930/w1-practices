2cfed4c11c27bf441475322a1410d8e5ccd37176
Dawid930 <davidnyikes@gmail.com>
2022-01-19 18:12:06 +0100

    adatstruktura-hozzadas-js-ben

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,54 @@
+/* const data = [
+    "Gergely Kis",
+    "Krisztian Bui",
+    "Buczko Balazs",
+    "Andras Varga",
+    "Barbala Terez Kovacs"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kis",
+        favouriteFood: "csirke/rizs"
+    },
+    {
+        name: "Andras Varga",
+        favouriteFood: "brassoi"
+    },
+    {
+        name: "Buczko Balazs",
+        favouriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "Krisztian Bui",
+        favouriteFood: "banan"
+    },
+    {
+        name: "Barbala Terez Kovacs",
+        favouriteFood: "avokado"
+    },
+    {
+        name: "Benett Viszokai",
+        favouriteFood: "alma"
+    },
+    {
+        name: "Krisztian Porneczi",
+        favouriteFood: "almas pite"
+    },
+]
 function loadEvent() {
     console.log("Az oldal betoltodott");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", ` 
-        <p>text</p>
-        `);
-        //elem.querySelector("p").style.fontFamily = `'Poppins', sans-serif`;
-        //elem.querySelector("h1").outerHTML = ""
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favouriteFood}</p>
+            </section>
+        `)
     }
 }
 window.addEventListener("load", loadEvent);
